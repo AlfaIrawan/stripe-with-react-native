da17b18aa68be43479b69e0ff71b1d21632fbde2
Alfa P. Irawan <alfa.irawan@outlook.com>
2020-04-19 17:45:33 +0700

    1.0.0 Add Android and IOS folder

---
+++ ios/CheckoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckoutViewController.h
+//  stripewithreactnative
+//
+//  Created by Alfa Irawan on 19/04/20.
+//
+
+#import <UIKit/UIKit.h>
+#import <Stripe/Stripe.h>
+
+@interface CheckoutViewController: UIViewController <STPAuthenticationContext>
+
+@end
---
+++ ios/StripeBridge.h
@@ -0,0 +1,18 @@
+//
+//  StripeBridge.h
+//  stripewithreactnative
+//
+//  Created by Alfa Irawan on 19/04/20.
+//
+
+#ifndef Stripe_h
+#define Stripe_h
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface StripeBridge : NSObject <RCTBridgeModule>
+@end
+
+
+#endif /* Stripe_h */
